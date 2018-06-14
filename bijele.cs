9118653c78992f8cedfee4269fca7ec565278312
Helen <helleskotta@gmail.com>
2018-06-14 11:00:51 +0200

    Bijele

---
+++ bijele.cs
@@ -0,0 +1,30 @@
+using System;
+
+// https://open.kattis.com/problems/bijele
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = Console.ReadLine().Split(' ');
+
+            var king = Convert.ToInt32(lines[0]); 
+            var queen = Convert.ToInt32(lines[1]); 
+            var rooks = Convert.ToInt32(lines[2]); 
+            var bishops = Convert.ToInt32(lines[3]); 
+            var knights = Convert.ToInt32(lines[4]); 
+            var pawns = Convert.ToInt32(lines[5]); 
+
+            king = (king == 0) ? 1 : -(king - 1);
+            queen = (queen == 0) ? 1 : -(queen - 1);
+            rooks = (rooks < 2) ? 2 - rooks : -(rooks - 2);
+            bishops = (bishops < 2) ? 2 - bishops : -(bishops - 2);
+            knights = (knights < 2) ? 2 - knights : -(knights - 2);
+            pawns = (pawns < 8) ? 8 - pawns : -(pawns - 8);
+
+            Console.WriteLine($"{king} {queen} {rooks} {bishops} {knights} {pawns}");
+        }
+    }
+}x
