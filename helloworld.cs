e5857207956c46dd4b2fb944b8b89e151333ffe5
Helen <helleskotta@gmail.com>
2018-06-14 10:57:51 +0200

    Hello World!

---
+++ helloworld.cs
@@ -0,0 +1,14 @@
+using System;
+
+// https://open.kattis.com/problems/hello
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
