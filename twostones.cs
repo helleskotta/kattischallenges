3cc3f15359f41aa998487459685e222c58fafe56
Helen <helleskotta@gmail.com>
2018-06-14 11:03:10 +0200

    Take Two Stones

---
+++ twostones.cs
@@ -0,0 +1,20 @@
+using System;
+
+// https://open.kattis.com/problems/twostones
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine()) % 2;
+
+            if (input == 0)
+                Console.WriteLine("Bob");
+
+            else
+                Console.WriteLine("Alice");
+        }
+    }
+}
