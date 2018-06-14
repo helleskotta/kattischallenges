bea65e249efc82a392c2f5b71f6fcc78dda6762f
Helen <helleskotta@gmail.com>
2018-06-14 11:10:24 +0200

    Solving for Carrots

---
+++ carrots.cs
@@ -0,0 +1,15 @@
+using System;
+
+// https://open.kattis.com/problems/carrots
+
+namespace Carrots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            Console.WriteLine(line[1]);
+        }
+    }
+}
